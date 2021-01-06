23d1d8607c7bce281c1777a934e75244af79a1e0
Lindy Merwin <lindymerwin@Lindys-Air.attlocal.net>
2021-01-06 15:54:00 -0800

    Final Push

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const boxes = document.querySelectorAll('box')
+const boxes = document.querySelectorAll('.box')
 
 
 window.addEventListener('scroll', checkBoxes)
@@ -9,6 +9,12 @@ function checkBoxes() {
   const triggerBottom = window.innerHeight / 5 * 4
   
   boxes.forEach(box => {
-    const boxTop = box.getBoundingClientRect()
+    const boxTop = box.getBoundingClientRect().top
+
+    if (boxTop < triggerBottom) {
+      box.classList.add('show')
+    } else {
+      box.classList.remove('show')
+    }
   })
 }
